39988124999e49558b4b4948fab6c5bf8f100482
Dante <dsdante1@yandex.ru>
2024-02-22 19:16:26 +0300

    Added Tinkoff API

--- TradingBot/Program.cs
+++ TradingBot/Program.cs
@@ -1,10 +1,14 @@
+using Tinkoff.InvestApi;
 using TradingBot;
 using TradingBot.Data;
 
 var builder = Host.CreateApplicationBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
 
-builder.Services.AddTradingBotDatabase(builder.Configuration);
-builder.Services.AddHostedService<Worker>();
+services.AddTradingBotDatabase(configuration);
+services.AddInvestApiClient((_, settings) => configuration.Bind("Tinkoff", settings));
+services.AddHostedService<Worker>();
 
 var host = builder.Build();
 host.Run();
--- TradingBot/Worker.cs
+++ TradingBot/Worker.cs
@@ -1,25 +1,37 @@
 using Microsoft.EntityFrameworkCore;
+using Tinkoff.InvestApi;
 using TradingBot.Data;
 
 namespace TradingBot;
 
-public class Worker(IServiceScopeFactory scopeFactory, ILogger<Worker> logger) : BackgroundService
+public class Worker(InvestApiClient tinkoff,
+                    IServiceScopeFactory scopeFactory,
+                    ILogger<Worker> logger,
+                    IHostApplicationLifetime lifetime) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken cancellation)
     {
-        await using (var scope = scopeFactory.CreateAsyncScope())
+        try
         {
-            var db = scope.ServiceProvider.GetRequiredService<TradingBotDbContext>();
-            var x = await db.Instruments
-                .AsNoTracking()
-                .FirstOrDefaultAsync(cancellation);
-            logger.LogInformation("{instrument}", x?.Name);
-        }
+            var shares = await tinkoff.Instruments.SharesAsync(cancellation);
+            logger.LogInformation("{instrument}", shares.Instruments.FirstOrDefault()?.Name);
 
-        while (!cancellation.IsCancellationRequested)
+            await using (var scope = scopeFactory.CreateAsyncScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<TradingBotDbContext>();
+                var x = await db.Instruments
+                    .OrderBy(instrument => instrument.Id)
+                    .Select(instrument => instrument.Name)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(cancellation);
+                logger.LogInformation("{instrument}", x);
+            }
+
+            lifetime.StopApplication();
+        }
+        catch (Exception e) when (e.InnerException is OperationCanceledException)  // Tinkoff API fix
         {
-            logger.LogInformation("Worker running.");
-            await Task.Delay(1000, cancellation);
+            throw e.InnerException;
         }
     }
 }
